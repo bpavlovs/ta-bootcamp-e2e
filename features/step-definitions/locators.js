11009fdc9aa8aa11830ec45703b009049497bbe8
bpavlovs <bogdans.pavlovs@gmail.com>
2022-12-01 19:14:42 +0200

    moving locators to a separate '.js' file

---
+++ features/step-definitions/locators.js
@@ -0,0 +1,10 @@
+
+module.exports = {
+    promoBanner: 'img[class="modal-Website-img"]',
+    closeButton: 'button[class="close"]',
+    searchField: 'input[type="search"]',
+    searchButton: 'button[class="ico ico-search"]',
+    bestDealsButton: 'a[id="trendingBanner_720202"]',
+    shopLogo: 'a[class="header2021-logo-img"]',
+    items: 'div[class="item-cell"]'
+};
